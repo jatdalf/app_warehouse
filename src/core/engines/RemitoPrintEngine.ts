57316aa85eea4ca4b6df2686baff0fd805c232db
Jatdalf <tu@correo.com>
2026-08-11 08:36:03 -0300

    remito numbers update II

--- src/core/engines/RemitoPrintEngine.ts
+++ src/core/engines/RemitoPrintEngine.ts
@@ -1,8 +1,6 @@
 import type { PipelineStep } from "../pipeline/PeYaPipeline";
 import type { EngineResult } from "../shared/EngineResult";
 import { WarehouseSession } from "../warehouse/WarehouseSession";
-
-import { RemitoFactory } from "../remitos/RemitoFactory";
 import { RemitoPrintService } from "../../services/remito/RemitoPrintService"
 
 export class RemitoPrintEngine
@@ -10,24 +8,11 @@ implements PipelineStep {
 
     readonly name = "Imprimiendo Remitos";
 
-    async execute(
-        session: WarehouseSession
-    ): Promise<EngineResult> {
-
-        const remitos =
-            RemitoFactory.build(
-                session.picking
-            );
-
-        RemitoPrintService.imprimir(
-            remitos
-        );
-
+    async execute(session: WarehouseSession): Promise<EngineResult> {
+        RemitoPrintService.imprimir(session.remitos);
         return {
             success: true,
             message: "Remitos enviados a impresión."
         };
-
     }
-
 }
